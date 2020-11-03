2a0187f7734beb5191ba2e7b85b6ede2e7cf6349
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2020-11-03 17:32:49 +0100

    Generated docs

--- src/common/Errors/ApiError.ts
+++ src/common/Errors/ApiError.ts
@@ -1,5 +1,10 @@
-export class ApiError extends Error{
-    public constructor(message: string){
+/**
+ * An error that occour when Api in App isn't initialized
+ */
+export class ApiError extends Error
+{
+    public constructor(message: string)
+    {
         super(message);
         this.message = message;
         this.name = ApiError.name;
