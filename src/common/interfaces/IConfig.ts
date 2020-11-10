43aa3bcec0b147e0bbab4ce7a005136254d61c87
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2020-11-11 00:52:15 +0100

    Updated config file

--- src/common/interfaces/IConfig.ts
+++ src/common/interfaces/IConfig.ts
@@ -1,8 +1,22 @@
 export interface IConfig {
     server: IServer;
+    fileSystem: IFileSystem;
+    localization: ILocalization;
 }
 
 export interface IServer{
     port: number;
     host: string;
+}
+
+export interface IFileSystem
+{
+    localizationConfig: string;
+    localizationFiles: string;
+    root: string;
+    controllers: string;
+}
+
+export interface ILocalization{
+    lang: string;
 }
