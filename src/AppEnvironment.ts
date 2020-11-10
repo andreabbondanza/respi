93f397aa061bb72aa1756d0b02c2cc34fee08f48
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2020-11-10 15:46:41 +0100

    Fixed bug for new controller instance

--- src/AppEnvironment.ts
+++ src/AppEnvironment.ts
@@ -1,13 +1,19 @@
 import fs from "fs";
 import path from "path";
-import { IApiController } from "./common/IApiController";
-import { IConfig } from "./common/IConfig";
+import { IApi } from "./common/interfaces/IApi";
+import { IApiController } from "./common/interfaces/IApiController";
+import { IConfig } from "./common/interfaces/IConfig";
+import { IRouteDefinition } from "./common/interfaces/IRouterDefinition";
 import { envConfig } from "./IEnvConfig";
 
 export class AppEnvironment
 {
-    public static loadConfig(): IConfig{
-        const file = fs.readFileSync(path.join(envConfig.root,"config.app.json"), {encoding: "utf8"});
+    /**
+     * Load settings from configuration file
+     */
+    public static loadConfig(): IConfig
+    {
+        const file = fs.readFileSync(path.join(envConfig.root, "config.app.json"), { encoding: "utf8" });
         let config: IConfig = JSON.parse(file);
         return config;
     }
@@ -18,10 +24,14 @@ export class AppEnvironment
     {
         const result: IApiController[] = [];
         const controllers = fs.readdirSync(envConfig.controllers).where(x => x.endsWith(".controller.js"));
-        for (const controller of controllers) {
-            const currController = require("./" + path.join(envConfig.controllers,controller));
-            result.push(new (currController.HomeController)());
+        for (const controller of controllers)
+        {
+            const currController = require("./" + path.join(envConfig.controllers, controller));
+            const name = Object.keys(currController)[0];
+            result.push(new (currController[name])());
         }
         return result;
     }
+
+  
 }
