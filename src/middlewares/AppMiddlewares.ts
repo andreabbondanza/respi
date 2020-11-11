5f912d24d51e78b563e940b000dafd94a280a81f
andreabbondanza <andreabbondanza.developer@outlook.com>
2020-11-11 11:34:29 +0100

    Fixed problem with const that init all middleware before the call

--- src/middlewares/AppMiddlewares.ts
+++ src/middlewares/AppMiddlewares.ts
@@ -1,13 +1,18 @@
 import { RequestHandler } from "express";
-import { LanguageManager, StorageServer } from "language-manager-ts";
 import { LoggerMiddleware } from "./LoggerMiddleware";
 import { ProduceJsonMiddleware } from "./ProduceJsonMiddleware";
+import { StaticFilesMiddleware } from "./StaticFilesMiddleware";
 
 /**
  * The list of middlewares (list take care of order)
  */
-export const middlewares: RequestHandler[] =
-    [
+
+export function middlewares(): RequestHandler[]
+{
+    return [
         new LoggerMiddleware().get(),
-        new ProduceJsonMiddleware().get()
+        new ProduceJsonMiddleware().get(),
+        new StaticFilesMiddleware().get()
     ];
+}
+
